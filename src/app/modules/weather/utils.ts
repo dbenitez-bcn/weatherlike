abc93c999bc0c46b1cf53a39fa59fe5477c8e464
David Benitez <dbr.bcn@gmail.com>
2019-04-12 00:13:14 +0200

    utils done

---
+++ src/app/modules/weather/utils.ts
@@ -0,0 +1,17 @@
+export const URLS = {
+  heroku: 'https://cors-anywhere.herokuapp.com/',
+  metaweatherapi: 'https://www.metaweather.com/api/'
+}
+
+export const weatherStates = {
+  sn: 'https://www.metaweather.com/static/img/weather/sn.svg',
+  sl: 'https://www.metaweather.com/static/img/weather/sl.svg',
+  h: 'https://www.metaweather.com/static/img/weather/h.svg',
+  t: 'https://www.metaweather.com/static/img/weather/t.svg',
+  hr: 'https://www.metaweather.com/static/img/weather/hr.svg',
+  lr: 'https://www.metaweather.com/static/img/weather/lr.svg',
+  s: 'https://www.metaweather.com/static/img/weather/s.svg',
+  hc: 'https://www.metaweather.com/static/img/weather/hc.svg',
+  lc: 'https://www.metaweather.com/static/img/weather/lc.svg',
+  c: 'https://www.metaweather.com/static/img/weather/c.svg'
+}
